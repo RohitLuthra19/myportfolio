10641f2c46c71fece866581d426eea8d1dcf90d4
Rohit Luthra <rohit.luthra19@gmail.com>
2021-06-26 18:47:01 +0530

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
+import Head from "next/head";
+
+import AppInfo from "../lib/app.json";
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<>
+  <Head>
+    <title>
+      {AppInfo?.appName?.toUpperCase()}
+    </title>
+    <meta charSet="UTF-8" />
+    <meta
+      name="description"
+      content="Rohit Luthra"
+    />
+    <meta
+      name="keywords"
+      content="Rohit Luthra"
+    />
+    <meta
+      name="author"
+      content={`${AppInfo.appName} || ${AppInfo.contactEmailId}`}
+    />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+    />
+    <link rel="icon" href="/favicon.ico" />
+  </Head>
+  <Component {...pageProps} />
+</>)
 }
 
 export default MyApp
---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet" />
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
