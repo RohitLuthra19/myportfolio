7e317ec9f09a27dbf7eaff0bf6834b4c24951722
Rohit Luthra <rohit.luthra19@gmail.com>
2021-06-27 22:37:43 +0530

    updated font

--- pages/_document.js
+++ pages/_document.js
@@ -10,9 +10,27 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-            <link rel="preconnect" href="https://fonts.gstatic.com" />
-            <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet" />
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+          {/* <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lato&display=swap"
+            rel="stylesheet"
+          /> */}
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+          />
+          <link
+            rel="preload"
+            href="/fonts/gdsherpa-vf.woff2"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/gd-sage-bold.woff2"
+            as="font"
+            crossOrigin=""
+          />
         </Head>
         <body>
           <Main />
