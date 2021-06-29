76b083a6232eaba5f3dc911d946dba6486c740d9
Rohit Luthra <rohit.luthra19@gmail.com>
2021-06-29 15:37:29 +0530

    created social svg icon components

---
+++ public/icons/stackOverflowIcon.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const StackOverflowIcon = ({ className }) => {
+  return (
+    <svg
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="21"
+    >
+      <g>
+        <title>Stack Overflow</title>
+        <g stroke="null" id="svg_1">
+          <path
+            stroke="null"
+            id="svg_2"
+            d="m16.79704,13.1871l0,5.16225l-14.31767,0l0,-5.16225l-2.15684,0l0,6.99402l18.62723,0l0,-6.99402l-2.15272,0z"
+            fill="#bcbbbb"
+          />
+          <path
+            stroke="null"
+            id="svg_3"
+            d="m4.4513,16.68412l10.38086,0l0,-1.66524l-10.38086,0l0,1.66524zm0.20745,-3.98243l10.0838,1.79746l0.41509,-1.70937l-10.08419,-1.79746l-0.4147,1.70937l-0.00001,0zm1.32802,-4.10582l9.33675,3.7005l0.87156,-1.58597l-9.33734,-3.7005l-0.87136,1.58597l0.00039,0zm2.5935,-3.89433l7.92598,5.60354l1.32802,-1.33919l-7.94696,-5.60354l-1.30704,1.33919l0.00001,0zm5.10425,-4.14112l-1.66017,1.03961l6.14169,7.03115l1.65978,-1.03978l-6.1413,-7.03099z"
+            fill="#f48024"
+          />
+        </g>
+      </g>
+    </svg>
+  );
+};
+
+export default StackOverflowIcon;
